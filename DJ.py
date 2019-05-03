295c253188be4d706694759d63ceec73d5f0a7a9
isso0424 <49902177+isso0424@users.noreply.github.com>
2019-05-03 12:27:36 +0900

    dj bot youtube data api ver 1.0

--- DJ.py
+++ DJ.py
@@ -1,11 +1,15 @@
 import discord
-from selenium import webdriver
-import chromedriver_binary
-from selenium.webdriver import Chrome, ChromeOptions
+from googleapiclient.discovery import build
+from oauth2client.tools import argparser
+import apiclient.discovery
+import apiclient.errors
+DEVELOPER_KEY = "AIzaSyDyXGfph5FZlo55Xa2W5gp-wbpt94WH2eI"
+YOUTUBE_API_SERVICE_NAME = "youtube" 
+YOUTUBE_API_VERSION = "v3"
 import youtube_dl
 import os
 import asyncio
-TOKEN = ""
+TOKEN = "NTcyODU3ODc2MzM5NDkwODI5.XMiZjQ.4a1iHpZzcHlTMITMLuBKUiOi2MY"
 youtube_url = "https://www.youtube.com/?gl=JP"
 discord_voice_channel_id = ""
 voice = None
@@ -13,6 +17,8 @@ player = None
 client = discord.Client()
 loop = asyncio.get_event_loop()
 a = 1
+b = 1
+user_list=[]
 def nextm(music_list):
     async def nex(music_list):
         CHANNEL_ID = 573056770025455636
@@ -22,18 +28,33 @@ def nextm(music_list):
             await channel.send("次の曲は")
         print(CHANNEL_ID)
     loop.run_until_complete(nex(music_list))
-async def saisei(msg,music_list):
+async def saisei(msg,music_list,b):
     CHANNEL_ID = 573031928857362452
     channel = client.get_channel(CHANNEL_ID)
-    youtube_url = "https://www.youtube.com/results?search_query="
-    serch_url = youtube_url + msg
-    options = ChromeOptions()
-    options.add_argument("--headless")
-    driver = Chrome(options=options)
-    driver.get(serch_url)
-    url_now = driver.find_element_by_id('thumbnail').get_attribute("href")
-    print(url_now)
-    await channel.send(url_now)
+    youtube_url = "https://www.youtube.com/watch?v="
+    def youtube_search(msg):
+        youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+        developerKey=DEVELOPER_KEY)
+        search_response = youtube.search().list(
+        q=msg,
+        part="id,snippet",
+        maxResults=3
+        ).execute()
+        videos = []
+        channels = []
+        playlists = []
+        for search_result in search_response.get("items", []):
+            if search_result["id"]["kind"] == "youtube#video":
+                videos.append("%s" % (search_result["id"]["videoId"]))
+        print("\n".join(videos), "\n")
+        print(videos)
+        word = videos[0]
+        print(word)
+        return word
+    ids = youtube_search(msg)
+    url= youtube_url + ids
+    print(url)
+    await channel.send(url)
     ydl_opts = {
     'format': 'bestaudio/best',
     'outtmpl':  "sample_music" + '.%(ext)s',
@@ -45,8 +66,17 @@ async def saisei(msg,music_list):
     ],
     }
     ydl = youtube_dl.YoutubeDL(ydl_opts)
-    info_dict = ydl.extract_info(url_now, download=True)
-    voice.play(discord.FFmpegPCMAudio('sample_music.mp3'),after = lambda e: nextm(music_list))
+    info_dict = ydl.extract_info(url, download=True)
+    voice.play(discord.FFmpegPCMAudio('sample_music.mp3'), after=lambda e: nextm(music_list))
+    if b == 1:
+        voice.source = discord.PCMVolumeTransformer(voice.source, volume=1.0)
+        voice.source.volume = 0.01
+        b += 1
+    try:
+        print(voice.source.volume)
+    except:
+        pass
+    return b
 @client.event
 async def on_ready():
     CHANNEL_ID = 573056770025455636
@@ -59,11 +89,14 @@ async def on_ready():
 async def on_message(message):
     global voice, player
     global music_list
+    global b
+    global user_list
     CHANNEL_ID = 573056770025455636
     channel = client.get_channel(CHANNEL_ID)
     msg = message.content
     mess = str(message)
     music_kind = mess.lstrip("起動")
+    print(user_list)
     try:
         print(music_list)
     except:
@@ -71,8 +104,12 @@ async def on_message(message):
     if message.author.bot:
         if msg == "次の曲は":
             nextmusic = music_list.pop(0)
-            await channel.send(nextmusic + "です")
-            loop.run_until_complete(saisei(nextmusic,music_list))
+            who_add = user_list.pop(0)
+            who_add = str(who_add)
+            print(who_add)
+            mssa = who_add +"が追加した" + nextmusic + "です"
+            await channel.send(mssa)
+            b = loop.run_until_complete(saisei(nextmusic,music_list,b))
             return music_list
     elif msg in "起動":
         if message.author.voice.channel is None:
@@ -96,7 +133,7 @@ async def on_message(message):
     elif msg == "停止":
         if voice.is_playing():
             voice.stop()
-            nextm(music_list)
+            music_list = []
             return
     elif msg == "切断":
         if voice is not None:
@@ -111,16 +148,18 @@ async def on_message(message):
                 music_list.append(msg)
             except:
                 music_list = [msg]
+            user_list.append(message.author.name)
             CHANNEL_ID = 573056770025455636
             channel = client.get_channel(CHANNEL_ID)
             await channel.send(msg + "を再生リストに追加しました")
+            
         else:
             music_list = None
-            loop.run_until_complete(saisei(msg,music_list))
+            b = loop.run_until_complete(saisei(msg, music_list,b))
     elif "リスト" == msg:
         count = 1
         for music in music_list:
-            senmess = str(count) + "曲目..." + music
+            senmess = str(count) + "曲目..." + music + +"("+user_list[count - 1] + ")"
             await channel.send(senmess)
             count += 1
     elif "音量" in msg:
@@ -135,5 +174,28 @@ async def on_message(message):
         except:
             pass
     elif "help" == msg:
-        await message.channel.send("起動/BOTをサーバーに呼ぶ\n? 曲名で曲を再生\n停止/曲を止める\n切断/BOTをボイスチャンネルから追い出す\nhelp/コマンド一覧を表示する\n移動/前に再生していた曲を維持してBOTをサーバーに呼ぶ")
+        await message.channel.send("起動/BOTをサーバーに呼ぶ\n次/次の曲に飛びます\n? 曲名で曲を再生\n停止/曲を止める(この時再生リストをリセットします\n切断/BOTをボイスチャンネルから追い出す\nhelp/コマンド一覧を表示する\n移動/前に再生していた曲を維持してBOTをサーバーに呼ぶ\nリセット/再生リストをリセットする\n音量 (半角スペース) 数値 /音量を現在の「数値」%にします")
+    elif msg == "次":
+        try:
+            voice.stop()
+            nextm(music_list)
+        except:
+            pass
+        return
+    elif msg == "リセット":
+        music_list = []
+        await channel.send("再生リストをリセットしました")
+
+    elif "削除" in msg:
+        msg = msg.replace("削除 ", "")
+        try:
+            msg = int(msg)
+            msg -= 1
+            try:
+                trash = music_list.pop(msg)
+                await channel.send(trash + "を再生リストから削除しました")
+            except:
+                pass
+        except:
+            pass
 client.run(TOKEN)
